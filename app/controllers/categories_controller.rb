ea65a643d86af7e114726619349decfe8a451a0e
sameedmohsin28 <sameedmohsin22@hotmail.com>
2023-08-25 02:28:16 +0500

    Write controllers and views for all pages

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,26 @@
 class CategoriesController < ApplicationController
   def index
-    @categories = Category.all
+    @categories = current_user.categories
   end
 
   def new
+    @category = Category.new
   end
 
   def create
+    @category = Category.new(category_params)
+    @category.user = current_user
+    if @category.save
+      redirect_to categories_path, notice: 'New category added'
+    else
+      flash.now[:alert] = 'Unable to add new category'
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :icon)
   end
 end
--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,11 +1,27 @@
 class ExpensesController < ApplicationController
   def index
-    @expenses = Expense.all
+    @category = Category.find(params[:category_id])
+    @expenses = @category.expenses
   end
 
   def new
+    @expense = Expense.new
   end
 
   def create
+    @category = Category.find(params[:category_id])
+    @expense = Expense.new(expense_params)
+    @expense.author = current_user
+    if @expense.save
+      @category.expenses << @expense
+      redirect_to category_expenses_path(@category), notice: 'New expense added'
+    else
+      flash.now[:alert] = 'Unable to add new category'
+      render :new
+    end
+  end
+
+  def expense_params
+    params.require(:expense).permit(:name, :amount)
   end
 end
