fbb2b39664a35629a4c02a12262f5497d8205fc3
sameedmohsin28 <sameedmohsin22@hotmail.com>
2023-08-24 19:49:30 +0500

    Add devise authentication

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   def index
-    @categories = User.find(2).categories
+    @categories = Category.all
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # get 'expense/index'
   # get 'expense/new'
   # get 'expense/create'
